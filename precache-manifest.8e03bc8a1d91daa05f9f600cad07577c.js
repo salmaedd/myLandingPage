0f5844398a88bbef90af8049dc25807ea61998d5
eddafour <salma.eddafour@capgemini.com>
2019-11-24 12:59:02 +0100

    Updates

---
+++ precache-manifest.8e03bc8a1d91daa05f9f600cad07577c.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6816c36b55ce1d6f0852bbd61478718e",
+    "url": "/myLandingPage/index.html"
+  },
+  {
+    "revision": "4ecbcbb8dee620fef72f",
+    "url": "/myLandingPage/static/css/2.a43d2b49.chunk.css"
+  },
+  {
+    "revision": "975e712f25b7059c070b",
+    "url": "/myLandingPage/static/css/main.a51bcca1.chunk.css"
+  },
+  {
+    "revision": "4ecbcbb8dee620fef72f",
+    "url": "/myLandingPage/static/js/2.94db42a4.chunk.js"
+  },
+  {
+    "revision": "975e712f25b7059c070b",
+    "url": "/myLandingPage/static/js/main.b089e1d4.chunk.js"
+  },
+  {
+    "revision": "2e562408fc05d5dd5759",
+    "url": "/myLandingPage/static/js/runtime-main.9e27a4ff.js"
+  },
+  {
+    "revision": "fa8687b954ce20de5957be3e757977a9",
+    "url": "/myLandingPage/static/media/Snowflake.fa8687b9.svg"
+  },
+  {
+    "revision": "b4e819d6c1ef32457dbb0a114762e55c",
+    "url": "/myLandingPage/static/media/archi.b4e819d6.jpg"
+  },
+  {
+    "revision": "4546528ed5b94ad8b2a3ec577c5300f4",
+    "url": "/myLandingPage/static/media/art-blank.4546528e.jpg"
+  },
+  {
+    "revision": "310f5f2ea448ab36e4bb54a17081c2ba",
+    "url": "/myLandingPage/static/media/art.310f5f2e.jpg"
+  },
+  {
+    "revision": "96b9acd64733235456efe56097efb7a3",
+    "url": "/myLandingPage/static/media/avatar.96b9acd6.png"
+  },
+  {
+    "revision": "dc60e17f24f0d03aa90054e2c4d94d70",
+    "url": "/myLandingPage/static/media/bg-2.dc60e17f.jpg"
+  },
+  {
+    "revision": "0c151f104c8f15f197684c2172d10e8c",
+    "url": "/myLandingPage/static/media/birdeyes.0c151f10.jpg"
+  },
+  {
+    "revision": "ac3e5ee1b318bf5d18c1098376760def",
+    "url": "/myLandingPage/static/media/black.ac3e5ee1.jpg"
+  },
+  {
+    "revision": "b1eabdfce26cdff22602d0535e8482ad",
+    "url": "/myLandingPage/static/media/bluecity.b1eabdfc.jpg"
+  },
+  {
+    "revision": "46fd4dfeaffb71b88f73cb74f7f53311",
+    "url": "/myLandingPage/static/media/brick.46fd4dfe.jpg"
+  },
+  {
+    "revision": "5f9d7e2c8f877dfa7ffd0e39ece20215",
+    "url": "/myLandingPage/static/media/close.5f9d7e2c.jpg"
+  },
+  {
+    "revision": "c509a4c46008514b5a5084d2596c99e4",
+    "url": "/myLandingPage/static/media/facebook.c509a4c4.svg"
+  },
+  {
+    "revision": "392322123bd62b044917201f2130200c",
+    "url": "/myLandingPage/static/media/golden.39232212.jpg"
+  },
+  {
+    "revision": "32b78dbd462232c4abafa14abde3de6e",
+    "url": "/myLandingPage/static/media/guit.32b78dbd.jpg"
+  },
+  {
+    "revision": "da26030608de4d7f38860dd4a77e953b",
+    "url": "/myLandingPage/static/media/happypic.da260306.jpg"
+  },
+  {
+    "revision": "7db6756a9f979246134cf298e3768ea9",
+    "url": "/myLandingPage/static/media/high.7db6756a.jpg"
+  },
+  {
+    "revision": "56bb631f21fe9c52f0c06b6df573b998",
+    "url": "/myLandingPage/static/media/highquality.56bb631f.jpg"
+  },
+  {
+    "revision": "cd42a0367e0842afdbbd92db270ad031",
+    "url": "/myLandingPage/static/media/instagram.cd42a036.png"
+  },
+  {
+    "revision": "14f7b6753cb58692c0da571c41b33210",
+    "url": "/myLandingPage/static/media/linkedin.14f7b675.svg"
+  },
+  {
+    "revision": "1a3259cc67d173c493804ab54dd21690",
+    "url": "/myLandingPage/static/media/modern.1a3259cc.jpg"
+  },
+  {
+    "revision": "9356ba8bebe18b65e0f638e6e4294eb4",
+    "url": "/myLandingPage/static/media/selective.9356ba8b.jpg"
+  },
+  {
+    "revision": "7bd1fc0c7a16ecc29ef3b2c95bbc847c",
+    "url": "/myLandingPage/static/media/sidebar_1.7bd1fc0c.png"
+  },
+  {
+    "revision": "50858175eb60301768419c4d52985694",
+    "url": "/myLandingPage/static/media/silhouette.50858175.jpg"
+  },
+  {
+    "revision": "a57ea219084451288a1f02aabba16f1b",
+    "url": "/myLandingPage/static/media/twitter.a57ea219.svg"
+  }
+]);
--- src/App.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-
-function App() {
-  return <div className="App"></div>;
-}
-
-export default App;
