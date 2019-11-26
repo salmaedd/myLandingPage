6774b65f1c5798e7c619ea5d8b323475858a1d7a
eddafour <salma.eddafour@capgemini.com>
2019-11-26 17:26:40 +0100

    fix: path /Home

--- src/components/widgets/MyNavbar.js
+++ src/components/widgets/MyNavbar.js
@@ -13,7 +13,10 @@ $(".nav-link").click(function() {
 function MyNavbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark ">
-      <a className="navbar-brand ml-5 font-weight-bold" href="/Home">
+      <a
+        className="navbar-brand ml-5 font-weight-bold"
+        href={process.env.PUBLIC_URL + "/Home"}
+      >
         <img src={Snowflake} alt="Snowflake" style={{ width: "30px" }} />
       </a>
 
